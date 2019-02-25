67823e9f5be04b3607e5a55208fff02f3562fa85
Pavel Didenko <adasslier@mail.ru>
2019-02-26 00:35:20 +0300

    Refactoring TodoListItem from class to function

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,44 +1,40 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import './todo-list-item.css';
 
-export default class TodoListItem extends Component {
+const TodoListItem = ({ label, onDeleted, onToggleDone, onToggleImportant, important, done }) => {
 
-  render() { 
-    const { label, onDeleted,
-          onToggleDone, onToggleImportant,
-          important, done } = this.props;
+  let classNames = 'todo-list-item';
 
-    let classNames = 'todo-list-item';
+  if (done) {
+    classNames += ' done';
+  }
 
-    if (done) {
-      classNames += ' done';
-    }
+  if (important) {
+    classNames += ' important';
+  }
 
-    if (important) {
-      classNames += ' important';
-    }
-  
-    return (
-      <span className={classNames}>
-        <span
-          className="todo-list-item-label"
-          onClick={onToggleDone}>
-          {label}
-        </span>
-  
-        <button type="button"
-                className="btn btn-outline-success btn-sm float-right"
-                onClick={onToggleImportant}>
-          <i className="fa fa-exclamation" />
-        </button>
-  
-        <button type="button"
-                className="btn btn-outline-danger btn-sm float-right"
-                onClick={onDeleted}>
-          <i className="fa fa-trash-o" />
-        </button>
+  return (
+    <span className={classNames}>
+      <span
+        className="todo-list-item-label"
+        onClick={onToggleDone}>
+        {label}
       </span>
-    );
-  };
+
+      <button type="button"
+              className="btn btn-outline-success btn-sm float-right"
+              onClick={onToggleImportant}>
+        <i className="fa fa-exclamation" />
+      </button>
+
+      <button type="button"
+              className="btn btn-outline-danger btn-sm float-right"
+              onClick={onDeleted}>
+        <i className="fa fa-trash-o" />
+      </button>
+    </span>
+  );
 };
+
+export default TodoListItem;
